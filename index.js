9a783bcc270ba60c3134d5351dd511a9bd9659d3
Rohan Kadam <kadamrohan76@gmail.com>
2020-10-17 18:58:18 +0530

    Sequence change in api sending response

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ const express=require('express');
 const path=require('path');
 const app=express();
 
-app.get("/",(req,res)=>{
-res.send("Hello There");
-});
-
 
 if(process.env.NODE_ENV==='production'){
     app.get("*",(req,res)=>{
 res.sendFile(path.resolve(__dirname,'client','build',"index.html"));
     })
 }
+
+
+app.get("/",(req,res)=>{
+    res.send("Hello There");
+    });
+    
 let port=process.env.PORT || 5000;
 app.listen(port,()=>{
     console.log(`Listening on port ${port}`);
