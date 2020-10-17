a2b0a0692d95f28383760ab40bf0634288b67d0e
Rohan Kadam <kadamrohan76@gmail.com>
2020-10-17 18:50:07 +0530

    whenin prod send index.html

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express=require('express');
-
+const path=require('path');
 const app=express();
 
 app.get("/",(req,res)=>{
 res.send("Hello There");
 });
 
+
+if(process.env.NODE_ENV==='production'){
+    app.get("/",(req,res)=>{
+res.sendFile(path.resolve(__dirname,'client','build',"index.html"));
+    })
+}
 let port=process.env.PORT || 5000;
 app.listen(port,()=>{
     console.log(`Listening on port ${port}`);
