5ea830315c8db24b28b3dc073ce869ad7a5b5ebf
Rohan Kadam <kadamrohan76@gmail.com>
2020-10-17 18:54:10 +0530

    try *

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ res.send("Hello There");
 
 
 if(process.env.NODE_ENV==='production'){
-    app.get("/",(req,res)=>{
+    app.get("*",(req,res)=>{
 res.sendFile(path.resolve(__dirname,'client','build',"index.html"));
     })
 }
